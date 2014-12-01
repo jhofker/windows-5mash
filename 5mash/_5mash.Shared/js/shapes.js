f25f7890dca279b117fdcbdd8ee88e0aec669424
Jordan Hofker <jordan.hofker@hudl.com>
2014-11-30 21:11:34 -0600

    Add a slight bit of rotation to the shapes.

--- 5mash/_5mash.Shared/js/shapes.js
+++ 5mash/_5mash.Shared/js/shapes.js
@@ -16,6 +16,7 @@
             offsetY = height / 2,
             newX = x - offsetX,
             newY = y - offsetY,
+            angle = Utils.getRandomNumber(0, 90),
             shape = new createjs.Shape();
 
         shape.graphics.beginStroke(strokeColor);
@@ -23,16 +24,16 @@
         shape.graphics.beginFill(fillColor);
         shape.snapToPixel = true;
 
-        var shapeRandom = Utils.getRandomNumber(0, 4);
+        var shapeRandom = Utils.getRandomNumber(0, 5);
 
         switch (shapeRandom) {
             case 0: //triangle
-                shape.graphics.drawPolyStar(0, 0, width / 2, 3, 0, 30);
+                shape.graphics.drawPolyStar(0, 0, width / 2, 3, 0, angle);
                 shape.x = x;
                 shape.y = y;
                 break;
             case 1: //5-point star
-                shape.graphics.drawPolyStar(0, 0, width / 2, 5, 0.6, -90);
+                shape.graphics.drawPolyStar(0, 0, width / 2, 5, 0.6, angle);
                 shape.x = x;
                 shape.y = y;
                 break;
@@ -40,19 +41,27 @@
                 shape.graphics.drawRoundRect(-width / 2, -height / 2, width, height, strokeStyle.thickness * 4);
                 shape.x = newX;
                 shape.y = newY;
+                shape.rotation = angle;
                 break;
             case 3: //circle
                 // circles draw around a point, so use the original x and y here.
                 shape.graphics.drawCircle(0, 0, width / 2);
                 shape.x = x;
                 shape.y = y;
+                shape.rotation = angle;
                 break;
             case 4: //rectangle
-            default:
                 shape.graphics.drawRect(-width / 2, -height / 2, width, height);
                 shape.x = newX;
                 shape.y = newY;
+                shape.rotation = angle;
                 break;
+            case 5: //pentagon
+                shape.graphics.drawPolyStar(0, 0, width / 2, 5, 0, angle);
+                shape.x = x;
+                shape.y = y;
+            default:
+                return shape;
         }
         shape.graphics.endStroke();
         return shape;
